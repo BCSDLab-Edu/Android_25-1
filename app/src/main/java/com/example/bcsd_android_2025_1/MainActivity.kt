66c641df7b8bfcae17aa550974e8492a03c0554d
Sangjin Kim <sjin070203@gmail.com>
2025-05-04 18:30:52 +0900

    5주차2

--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -5,10 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_random: Button = findViewById(R.id.button_random)
+        val button_count: Button = findViewById(R.id.button_count)
+        val button_toast: Button = findViewById(R.id.button_toast)
+
+        val textview_number: TextView = findViewById((R.id.textview_number))
+
+        var count = 0
+
+        button_toast.setOnClickListener {
+            Toast.makeText(this, "피슝파슝푸슝", Toast.LENGTH_SHORT).show()
+        }
+
+        button_count.setOnClickListener {
+            count++
+            textview_number.text = count.toString()
+        }
+
+        button_random.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            val random_number = Random().nextInt(count+1)
+            intent.putExtra("count_number", random_number.toString())
+            startActivity(intent)  // 화면 전환을 시켜줌
+            count = random_number
+            textview_number.text = random_number.toString()
+        }
     }
 }
--- app/src/main/java/com/example/bcsd_android_2025_1/SecondActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/SecondActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,16 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textview_second_number: TextView = findViewById(R.id.textview_second_number)
+        val textview_second_information: TextView = findViewById(R.id.textview_second_information)
+
+        var information = "Here is a random number between 0 and "
+
+        if (intent.hasExtra("count_number")) {
+            textview_second_number.text = intent.getStringExtra("count_number")
+            textview_second_information.text = information.plus(intent.getStringExtra("count_number"))
         }
     }
 }
