624391e95a0bc3a0887e9445e3479d2c1d10ac9e
Kimeunseo <hyunice1226@gmail.com>
2025-05-05 12:00:08 +0900

    assignment_05_toast

--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bcsd_android_2025_1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_login_main)
+        setSupportActionBar(toolbar)
+
+        val toastButton = findViewById<Button>(R.id.button_login_toast)
+        toastButton.setOnClickListener {
+            Toast.makeText(this, "와우!!", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
