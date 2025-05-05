d58f49be498f93d31448bedb4e15b239c7fe0b2a
Kimeunseo <hyunice1226@gmail.com>
2025-05-05 20:18:07 +0900

    assignment_05

--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.bcsd_android_2025_1
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    var number = 0
+    lateinit var textView: TextView
+
+    private val getRandomNumber =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val randomNumber = data?.getIntExtra("random", number)
+                if (randomNumber != null) {
+                    number = randomNumber
+                    textView.text = number.toString()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +38,21 @@ class MainActivity : AppCompatActivity() {
         toastButton.setOnClickListener {
             Toast.makeText(this, "와우!!", Toast.LENGTH_SHORT).show()
         }
+
+        textView = findViewById(R.id.text_login_number)
+        val countButton = findViewById<Button>(R.id.button_login_count)
+        countButton.setOnClickListener {
+            number++
+            textView.text = number.toString()
+        }
+
+        val randomButton = findViewById<Button>(R.id.button_login_random)
+        randomButton.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("number", number)
+            getRandomNumber.launch(intent)
+        }
+
     }
 
 }
