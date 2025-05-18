aad9bf526f9a735dee283f6fb1b7d4c2625d2b81
kimseokhyeon1210 <ceokim1210@naver,com>
2025-05-18 18:11:42 +0900

    7's assingment

--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.bcsd_android_2025_1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: RecyclerViewAdapter
+    private val nameList = mutableListOf<String>()
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.recyclerview)
 
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, FirstFragment())
-            .commit()
+        val input = findViewById<EditText>(R.id.input_name)
+        val add = findViewById<FloatingActionButton>(R.id.add_name)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_name)
+
+        adapter = RecyclerViewAdapter(this, nameList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        add.setOnClickListener {
+            val name = input.text.toString().trim()
+            if (name.isNotEmpty()) {
+                adapter.add_name(name)
+                input.text.clear()
+            }
+        }
     }
 }
