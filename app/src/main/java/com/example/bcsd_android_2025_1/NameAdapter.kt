7d731d3cfc855ff6e343ade4f6b99b78df381617
StayTogetherAllDay <seysey1473@naver.com>
2025-05-19 22:01:38 +0900

    DM

--- app/src/main/java/com/example/bcsd_android_2025_1/NameAdapter.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/NameAdapter.kt
@@ -1,20 +1,43 @@
-package com.example.bcsd_android_2025_1
+package com.example.namelistapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class NameAdapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_name_adapter)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+import android.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class NameAdapter(
+    private val names: MutableList<String>,
+    private val onItemClicked: (Int) -> Unit,
+    private val onItemLongClicked: (Int) -> Unit
+) : RecyclerView.Adapter<NameAdapter.NameViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NameViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.simple_list_item_1, parent, false)
+        return NameViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NameViewHolder, position: Int) {
+        holder.bind(names[position])
+        holder.itemView.setOnClickListener { onItemClicked(position) }
+        holder.itemView.setOnLongClickListener {
+            onItemLongClicked(position)
+            true
+        }
+    }
+
+    override fun getItemCount() = names.size
+
+    inner class NameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(android.R.id.text1)
+
+        fun bind(name: String) {
+            textView.text = name
         }
     }
 }
