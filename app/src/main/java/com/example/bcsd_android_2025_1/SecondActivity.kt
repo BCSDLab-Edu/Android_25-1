7df5134c84606a696610c32a7a279295a0a95213
StayTogetherAllDay <seysey1473@naver.com>
2025-05-26 19:32:27 +0900

    SUPER

--- app/src/main/java/com/example/bcsd_android_2025_1/SecondActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/SecondActivity.kt
@@ -1,25 +1,40 @@
 package com.example.bcsd_android_2025_1
 
+
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    private var randomValue: Int = -1
+    private var notificationId: Int = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         val count = intent.getIntExtra("count_value", 0)
+        notificationId = intent.getIntExtra("notification_id", -1)
 
-        val randomValue = (0 until count+1).random()
+
+        if (notificationId != -1) {
+            NotificationManagerCompat.from(this).cancel(notificationId)
+        }
+        randomValue = (0 until count + 1).random()
 
         val textView = findViewById<TextView>(R.id.Count)
         textView.text = "$randomValue"
+    }
 
-        val resultIntent = Intent()
-        resultIntent.putExtra("random_value", randomValue)
+    override fun onBackPressed() {
 
-        setResult(RESULT_OK, resultIntent)
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("random_value", randomValue)
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(intent)
+        super.onBackPressed()
     }
 }
