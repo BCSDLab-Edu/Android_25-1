24d4289ee6ca5be6a6b3d143623ce33b08cabab3
Kimeunseo <hyunice1226@gmail.com>
2025-07-08 02:33:44 +0900

    start <-> pause

--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.bcsd_android_2025_1
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.main_button_start)
+
+        startButton.setOnClickListener {
+            isRunning = !isRunning
+            if (isRunning) {
+                startButton.text = "Pause"
+            } else {
+                startButton.text = "Start"
+            }
+        }
     }
 }
