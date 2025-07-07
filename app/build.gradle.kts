9383d30f8e0125431405982d3875f4ca914358f9
Kimeunseo <hyunice1226@gmail.com>
2025-07-08 03:27:10 +0900

    타이머 구현 (start만)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,9 @@ android {
 }
 
 dependencies {
+//coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
+++ app/src/main/java/com/example/bcsd_android_2025_1/MainActivity.kt
@@ -1,30 +1,70 @@
 package com.example.bcsd_android_2025_1
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var timerText: TextView
     private lateinit var startButton: Button
+
     private var isRunning = false
+    private var startTime = 0L // 시작시간
+    private var beforeTime = 0L // 누적시간
+    private var timerJob: Job? = null // 중단 위함
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timerText = findViewById(R.id.main_text_timer)
         startButton = findViewById(R.id.main_button_start)
 
         startButton.setOnClickListener {
-            isRunning = !isRunning
-            if (isRunning) {
-                startButton.text = "Pause"
+            if (!isRunning) {
+                startTimer()
             } else {
-                startButton.text = "Start"
+                pauseTimer()
             }
         }
+    }
+
+    private fun startTimer() {
+        isRunning = true
+        startButton.text = "Pause"
+        startTime = SystemClock.elapsedRealtime() // 시작시점
+
+        timerJob = lifecycleScope.launch {
+            while (isRunning) {
+                val now = SystemClock.elapsedRealtime()
+                val totalTime = now - startTime + beforeTime  // 총 누적 시간 = 현재 시간 - 시작시간 + 이전누적시간
+                timerText.text = formatTime(totalTime)
+                delay(10)  // 10ms마다 갱신
+            }
+        }
+    }
+
+    private fun pauseTimer() {
+        isRunning = false
+        beforeTime += SystemClock.elapsedRealtime() - startTime  // 이전 누적 시간에 이번에 흐른 시간 더함.
+        timerJob?.cancel()
+        startButton.text = "Start"
+    }
+
+    private fun formatTime(ms: Long): String {
+        val minutes = ms / 1000 / 60
+        val seconds = (ms / 1000) % 60
+        val millis = (ms % 1000) / 10
+        return String.format("%02d : %02d : %02d", minutes, seconds, millis)
     }
 }
